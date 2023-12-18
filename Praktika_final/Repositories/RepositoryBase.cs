114564e573e29fa2813b6e435c418436b58f392c
Sergey_Maslyannikov <serg.maslyannikov@gmail.com>
2023-12-19 01:16:26 +0800

    Добавлены классы MVVM паттерна В этих классах содержатся методы и переменные для: -Управления пользователями -Хранение информации о пользователях.

---
+++ Praktika_final/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_final.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=FR3Y; Database=Ratanushka82; Integrated Security=true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
