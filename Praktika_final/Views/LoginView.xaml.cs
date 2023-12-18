0bf2923fb0515a99da876316706f67d25a7a43ae
Sergey_Maslyannikov <serg.maslyannikov@gmail.com>
2023-12-19 01:32:45 +0800

    Добавлена C# логика к окну MainView и LoginView

--- Praktika_final/Views/LoginView.xaml.cs
+++ Praktika_final/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Praktika_final.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,41 @@ namespace Praktika_final
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click_1(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Activate();
+            this.Close();
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Activate();
+            this.Close();
+        }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings f1 = new Settings();
+            f1.Show();
         }
     }
 }
--- Praktika_final/Views/MainView.xaml.cs
+++ Praktika_final/Views/MainView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Praktika_final.Properties;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,87 @@ namespace Praktika_final.Views
         public MainView()
         {
             InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            hellouser.Visibility = Visibility.Visible;
+            database.Visibility = Visibility.Hidden;
+            btnRefresh.Visibility = Visibility.Hidden;
+            btnReSort.Visibility = Visibility.Hidden;
+        }
+
+        private void Product_Click(object sender, RoutedEventArgs e)
+        {
+            hellouser.Visibility = Visibility.Hidden;
+            database.Visibility = Visibility.Visible;
+            btnRefresh.Visibility = Visibility.Visible;
+            btnReSort.Visibility = Visibility.Visible;
+            using (SqlConnection connection = new SqlConnection("Server=FR3Y; Database= Ratanushka82; Integrated Security=true"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM [Товары]", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                database.ItemsSource = dataTable.DefaultView;
+            }
+        }
+
+        private void goback_Click(object sender, RoutedEventArgs e)
+        {
+            var logv = new LoginView();
+            logv.Show();
+            this.Close();
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server=FR3Y; Database= Ratanushka82; Integrated Security=true");
+            connection.Open();
+            string cmd = "SELECT * FROM [Товары]";
+            SqlCommand createCommand = new SqlCommand(cmd, connection);
+            createCommand.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
+            DataTable dt = new DataTable("Товары");
+            dataAdp.Fill(dt);
+            database.ItemsSource = dt.DefaultView;
+            connection.Close();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings f1 = new Settings();
+            f1.Show();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnReSort_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server=FR3Y; Database= Ratanushka82; Integrated Security=true");
+            connection.Open();
+            string cmd = "SELECT * FROM [Товары] order by Стоимость";
+            SqlCommand createCommand = new SqlCommand(cmd, connection);
+            createCommand.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
+            DataTable dt = new DataTable("Товары");
+            dataAdp.Fill(dt);
+            database.ItemsSource = dt.DefaultView;
+            connection.Close();
         }
     }
 }
