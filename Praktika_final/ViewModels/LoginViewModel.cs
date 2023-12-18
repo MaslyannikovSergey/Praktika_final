3838b88247e4505be9ba8faffff3690e6a7205ac
Sergey_Maslyannikov <serg.maslyannikov@gmail.com>
2023-12-19 01:24:13 +0800

    К уже имеющимся компонентам добавлен компонент MVVM ViewModels(создан в отельной папке как и предыдущие) Классы в  нем служат хранением и манипуляцией с данными-представлениями в программе.

---
+++ Praktika_final/ViewModels/LoginViewModel.cs
@@ -0,0 +1,142 @@
+﻿using Praktika_final.Models;
+using Praktika_final.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Praktika_final.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Поля
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        private IUserRepository userRepository;
+
+        //Свойства полей
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public bool IsViewVisible
+        {
+            get
+            {
+                return _isViewVisible;
+            }
+
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        // Команды "под капотом"
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Конструктор
+        public LoginViewModel()
+        {
+            userRepository = new UserRepository();
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassCommand("", ""));
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 ||
+                Password == null || Password.Length < 3)
+                validData = false;
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            var isValidUser = userRepository.AuthenticateUser(new NetworkCredential(Username, Password));
+            var CurrentStatus = userRepository.GetStatus(new NetworkCredential(Username, Password));
+            if (isValidUser && CurrentStatus == "user")
+            {
+                Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username), null);
+                IsViewVisible = false;
+                MainView mv = new MainView();
+                mv.Show();
+
+
+            }
+            else if (isValidUser && CurrentStatus == "admin")
+            {
+                Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username), null);
+                IsViewVisible = false;
+                Admin admin = new Admin();
+                admin.Show();
+
+
+            }
+            else
+            {
+                ErrorMessage = "* Не правильно введено имя пользователя или пароль";
+            }
+
+        }
+
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Praktika_final/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Praktika_final.Models;
+using Praktika_final.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Praktika_final.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Поля
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Добро пожаловать {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+
+            }
+        }
+    }
+}
